748f46b43799c7d2f82d68a512610e7e1a5aa567
Jose Hernandez <reyes_7374@outlook.es>
2024-07-28 03:19:36 -0600

    modificacion en el gitignore

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8888/restapi2/public';
+    public string $baseURL = 'http://localhost:8888/restapicetec/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
